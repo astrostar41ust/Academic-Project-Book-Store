506a153d6457e0d468d33096ff61a2822ba7745d
astrostar41ust <astrostar41ust@gmail.com>
2025-10-06 22:49:04 +0700

    update order_routes function

--- backend/routes/order_routes.py
+++ backend/routes/order_routes.py
@@ -17,12 +17,17 @@ def list_orders():
 
 
 @order_bp.route("/<int:order_id>", methods=["GET"])
+@jwt_required()
 def get_order(order_id):
 
+    user_id = get_jwt_identity()
     order = db.session.get(Order, order_id)
 
     if not order:
         return jsonify({"msg": "Order not found"}), 404
+    
+    if order.user_id != user_id:
+        return jsonify({"msg": "Access forbidden: You can only view your own orders"}), 403
 
     return jsonify(order.to_dict()), 200
 
@@ -77,3 +82,43 @@ def place_order():
     except Exception as e:
         db.session.rollback()
         return jsonify({"msg": "Error placing order", "error": str(e)}), 500
+
+@order_bp.route('/admin/all', methods=['GET'])
+@jwt_required()
+@requires_roles('admin')
+def get_all_orders_admin():
+
+    all_orders = Order.query.order_by(Order.order_date.desc()).all()
+    
+    return jsonify([order.to_dict() for order in all_orders]), 200
+
+
+
+@order_bp.route('/admin/status/<int:order_id>', methods=['PUT'])
+@jwt_required()
+@requires_roles('admin')
+def update_order_status(order_id):
+    
+    data = request.get_json()
+    new_status = data.get('status')
+    
+    if not new_status:
+        return jsonify({"msg": "New status is required"}), 400
+    
+    if new_status not in ['Pending', 'Completed', 'Cancelled']:
+        return jsonify({"msg": "Invalid status value (Pending, Completed, Cancelled)"}), 400
+    
+    order = db.session.get(Order, order_id)
+    
+    if not order:
+        return jsonify({"msg": "Order not found"}), 404
+    
+    try:
+        order.status = new_status
+        db.session.commit()
+        
+        return jsonify({"msg": "Order status updated", "order": order.to_dict()}), 200
+    
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"msg": "Error updating order status", "error": str(e)}), 500
