97c6b3cd7dd6488b1e6fc370df6bdc7a731eaf45
EsterShiro <kittiwat.bankcat@gmail.com>
2025-11-27 06:00:19 +0700

    add image

--- admin/src/context/BookContext.tsx
+++ admin/src/context/BookContext.tsx
@@ -29,7 +29,7 @@ const BookProvider: React.FC<BookProviderProps> = ({ children }) => {
 
   const getBooks = async (): Promise<void> => {
     try {
-      const response = await fetch("/api/books/");
+      const response = await fetch(" http://localhost:5173/api/books/");
       if (!response.ok) throw new Error("Failed to fetch books");
 
       const data: Book[] = await response.json();
